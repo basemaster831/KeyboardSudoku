f3cdcfdc837fe5f6cee5ad8b691956f3697af2f9
basemaster831 <user@disp4150>
2021-03-03 18:57:18 +0000

    Cleaned up

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var board = document.getElementById("board");
-const TABLE_POSITION_ARRAY = [7, 8, 9, 4, 5, 6, 1, 2, 3];
-var cursor = {
+
+// Static grid array for numpad layout.
+const GRID_POSITION_ARRAY = [7,8,9,4,5,6,1,2,3];
+
+// The Sudoku Grid 
+var grid = document.getElementById("sudoku");
+
+//TODO implement difficulty
+var difficulty = ""
+
+var cursor = { 
     block: 0,
     tile: 0
 };
 
-window.onload = function () {
-    getId(STARTBUTTON).addEventListener("click", startGame);
-    updateMove();
+function startGame() {
+    // Triggered by the DOM event onclick, specified in HTML file.
 }
 
 window.addEventListener("keydown", event => {
@@ -17,12 +24,32 @@ window.addEventListener("keydown", event => {
 });
 
 // Highlight the block if no block is selected, otherwise it will be a tile
-function cursor_pointer(number) {
-    let block = document.getElementById(number);
-    if (cursor.tile) {
-        let tile = document.getElementById("t" + cursor.block + cursor.tile);
-    }
-    // TODO keep going here
+function cursorPointer(number) {
+    // ternary operator; if we have block selected,
+    // let block = tile, else block = block.
+
+    let block = cursor.block ?
+    document.getElementById("t"+cursor.block+cursor.tile) :
+    document.getElementById(number);
+
+    block.classList.add("highlight");
+    // We don't have a block selected, select the block.
+    if(!cursor.block) {
+        block.classList.add("highlight");    
+    } else if (cursor.block) {
+        // We're now selecting the tile but before we must
+        // remove the highlight class from the parent.
+        block.parentNode.classList.remove("highlight");
+        block.classList.add("highlight");
+    } else if(cursor.block && cursor.tile) {
+        // We have the block and tile selected, time to enter value
+        // EXCEPTION: if value is 0 then we delete the value from the tile
+        number == "0" ? tile.textContent = "" : tile.textContent = number;
+
+	cursor.block = 0;
+	cursor.tile = 0;
+    }   
+
 }
 
 // TODO create createBlock()
@@ -37,4 +64,4 @@ function generateTable(sudokuSize) {
     return sudokuGrid;
 }
 
-board.innerHTML = generateTable(9);
+grid.innerHTML = generateTable(9);
